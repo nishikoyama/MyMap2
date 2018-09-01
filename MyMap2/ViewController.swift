25a076dcb59ec09a21982fffd9d78c64bc0b0389
nishiko <nishiko1119@gmail.com>
2018-09-01 19:47:36 +0900

    Initial Commit

---
+++ MyMap2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyMap2
+//
+//  Created by 西山明也斗 on 2018/09/01.
+//  Copyright © 2018年 nishikoyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
